cf7669135b8abf44a00c2d97eff4ba8804a466bf
luis <luis.lozano.sanz43@gmail.com>
2025-05-16 09:34:19 +0200

    dwd

--- la_ultima_y_nos_vamos/src/app.py
+++ la_ultima_y_nos_vamos/src/app.py
@@ -1,12 +1,1 @@
-# app.py
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Welcome to the Voting Platform!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# ...empty file...
